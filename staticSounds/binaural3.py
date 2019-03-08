e42c964940766797a72a92874102c43605278555
Renato Fabbri <renato.fabbri@gmail.com>
2019-03-08 08:45:47 -0300

    melhora na rotina de renderizacao dos audio binaurais: saw e flac

--- staticSounds/binaural3.py
+++ staticSounds/binaural3.py
@@ -1,32 +1,43 @@
-import numpy as n
+import numpy as n, music as m, os
 from scipy.io import wavfile as w
-import music as m
 H = m.utils.H
 V = m.utils.V
 
-def mkBin(fl=[200], fr=[240], a=[.8], aa=.8, t=10, fname='binaural', sr=44100):
+def mkBin(fl=[200], fr=[240], a=[.8], aa=.8, t=10, fname='binaural', sr=44100, form='sine'):
     ts = 60*t
     if len(a) != len(fl):
         a = a*len(fl)
     sl = n.zeros(ts*sr)
-    for f, a_ in zip(fl, a):
-        s = n.linspace(0, ts*f*2*n.pi, ts*sr, endpoint=False)
-        sl += n.sin(s) * a_
     sr_ = n.zeros(ts*sr)
-    for f, a_ in zip(fr, a):
-        s = n.linspace(0, ts*f*2*n.pi, ts*sr, endpoint=False)
-        sr_ += n.sin(s) * a_
+    if form == 'sine':
+        for f, a_ in zip(fl, a):
+            s = n.linspace(0, ts*f*2*n.pi, ts*sr, endpoint=False)
+            sl += n.sin(s) * a_
+        for f, a_ in zip(fr, a):
+            s = n.linspace(0, ts*f*2*n.pi, ts*sr, endpoint=False)
+            sr_ += n.sin(s) * a_
+    elif form == 'saw':
+        for f, a_ in zip(fl, a):
+            sl += a_ * (n.linspace(0, ts*f*2, ts*sr)%2 * 2 - 1)
+        for f, a_ in zip(fr, a):
+            sr_ = a_ * (n.linspace(0, ts*f*2, ts*sr)%2 * 2 - 1)
 
     sl_ = aa * 2 * ( (sl - sl.min()) / (sl.max() - sl.min()) -0.5 )
     sr_ = aa * 2 * ( (sr_ - sr_.min()) / (sr_.max() - sr_.min()) -0.5 )
 
     w.write(fname + '.wav', sr, V(sl_, sr_).T)
+    os.system('sox ' + fname + '.wav ' + fname + '.flac')
+    os.system('rm ' + fname + '.wav')
+
     w.write(fname + '_.wav', sr, (sl_+sr_)*.5)
+    os.system('sox ' + fname + '_.wav ' + fname + '_.flac')
+    os.system('rm ' + fname + '_.wav')
 
 mkBin(
-    [100, 240, 400, 840, 1200],
-    [140, 200, 440, 800, 1240],
+    [30],
+    [30.2],
     [1, .8, .6, .4, .2],
-    fname='bin6',
-    sr=192000
+    fname='saw0.2hz01',
+    sr=192000,
+    form='saw'
 )
